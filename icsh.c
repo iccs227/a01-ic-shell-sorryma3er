a8e1b6c643bd11229f48dec36a74ed7ce2a88cac
sorryma3er <whc0713air@gmail.com>
2025-05-05 18:59:06 +0700

    Added echo feature and refactor code by putting feature into functions outside of main()

--- icsh.c
+++ icsh.c
@@ -9,44 +9,70 @@
 
 #define MAX_CMD_BUFFER 255 // maximum length of input; final
 
+//refactor out by features to make the main() cleaner
+void handle_exit(char *buffer);
+void handle_echo(char *buffer);
+
 int main() {
     char buffer[MAX_CMD_BUFFER]; // where we store the user input
 
     printf("Starting IC shell! Type commands down below\n"); // welcome message before REPL
 
+    //REPL
     while (1) {
         printf("icsh $ "); // prompt symbol
         
         if (!fgets(buffer, MAX_CMD_BUFFER, stdin)) break; // read and put the input into buffer
-
         buffer[strcspn(buffer, "\n")] = '\0'; // remove the trailing newline '\n' character
 
         if (strlen(buffer) == 0) continue; // if input is empty, go to next round
 
         printf("you said: %s\n", buffer);
 
-        //handle normal exit
-        if (strcmp(buffer, "exit") == 0) {
-            printf("Bye!\n");
-            exit(0);
+        char buffer_cp[MAX_CMD_BUFFER];
+        strcpy(buffer_cp, buffer);// make a copy here for func use later
+        printf("copy is: %s\n", buffer_cp);
+        char *token = strtok(buffer_cp, " "); // tokenize from the copy wont affect ori buffer
+
+        if (strcmp(token, "exit") == 0) {
+            handle_exit(buffer); 
+        } else if (strcmp(token, "echo") == 0) {
+            handle_echo(buffer);
+        } else {
+            printf("bad command!\n");
         }
-
-        //handle exit wwith code
-        char *token = strtok(buffer, " ");
-        if (token && strcmp(token, "exit") == 0) {
-            int code = 0;
-            char *code_char = strtok(NULL, " "); // passing NULL to it will continue parse the ori string
-
-            if (code_char) code = atoi(code_char) % 256;
-            if (code < 0) code += 256; // deal with neg exit code
-
-            printf("Bye!\n");
-            printf("exit code is: %i\n", code);
-            exit(code);
-        }
-        
-
     }
 
     return 0;
+}
+
+void handle_exit(char *buffer) {
+    char copy[MAX_CMD_BUFFER];
+    strcpy(copy, buffer);
+
+    //skip the exit word
+    strtok(copy, " ");
+    char *code_str = strtok(NULL, " ");
+    printf("code char is %s\n", code_str);
+
+    if (!code_str) { //no exit code
+        printf("here\n");
+        printf("Bye!\n");
+        exit(0);
+    } else {
+        int code = atoi(code_str) % 256;
+        if (code < 0) code += 256;
+        
+        printf("Bye!\n");
+        exit(code);
+    }
+}
+
+void handle_echo(char *buffer) {
+    char *to_print = buffer + 5; //buffer here is an addr. +5 will skip 'echo '
+    if (strlen(to_print) > 0) {
+        printf("%s\n", to_print);
+    } else {
+        printf("\n");
+    }
 }
