73d214c83939d16bfb07a3b9d6defdda1fcba98e
sorryma3er <whc0713air@gmail.com>
2025-05-06 18:24:36 +0700

    Added the logic to process last command, and refactored the process logic into a func so can reuse later

--- icsh.c
+++ icsh.c
@@ -13,6 +13,7 @@
 void handle_exit(char *buffer);
 void handle_echo(char *buffer);
 void handle_double_bang(char **last_cmd);
+void process_cmd(char *command, char **last_cmd);
 
 int main() {
     char buffer[MAX_CMD_BUFFER]; // where we store the user input
@@ -29,30 +30,10 @@ int main() {
 
         if (strlen(buffer) == 0) continue; // if input is empty, go to next round
 
-        printf("you said: %s\n", buffer);
-
-        char buffer_cp[MAX_CMD_BUFFER];
-        strcpy(buffer_cp, buffer);// make a copy here for func use later
-
-        char *token = strtok(buffer_cp, " "); // tokenize from the copy wont affect ori buffer
-
-        if (strcmp(token, "exit") == 0) {
-            handle_exit(buffer); 
-        } else if (strcmp(token, "echo") == 0) {
-            handle_echo(buffer);
-        } else if (strcmp(token, "!!") == 0){
-            handle_double_bang(&last_cmd);
-        } else {
-            printf("bad command!\n");
-        }
-
-        // update last command
-        if (strcmp(buffer, "!!") != 0) {
-            if (last_cmd) free(last_cmd);
-            last_cmd = strdup(buffer);
-        }
+        process_cmd(buffer, &last_cmd);
     }
 
+    if (last_cmd) free(last_cmd);
     return 0;
 }
 
@@ -92,6 +73,34 @@ void handle_double_bang(char **last_cmd) {
     }
 
     printf("%s\n", *last_cmd);
+    process_cmd(*last_cmd, last_cmd); // wrap up processing logics into a chunk, reuse in both main() & double_bang()
+}
 
-    // TODO: actual logic to process last cmd:
+void process_cmd(char *command, char **last_cmd) {
+    //printf("you said: %s\n", command);
+
+    char cmd_copy[MAX_CMD_BUFFER];// make a cp for command
+    strcpy(cmd_copy, command);
+    //printf("copy is: %s\n", cmd_copy);
+
+    char *token = strtok(cmd_copy, " ");// get first token from copy, so ori command field will stay the same
+    if (!token) return;
+
+    if (strcmp(token, "exit") == 0) {
+        handle_exit(command);
+    } else if (strcmp(token, "echo") == 0) {
+        handle_echo(command);
+    } else if (strcmp(command, "!!") == 0) {
+        handle_double_bang(last_cmd);
+    } else {
+        printf("bad command!\n");
+    }
+
+    if (strcmp(token, "!!") != 0) { // update the last_cmd field
+
+        if (*last_cmd) free(*last_cmd); // prevent mem leak
+
+        //printf("last command before update is %s\n", command);
+        *last_cmd = strdup(command);
+    }
 }
